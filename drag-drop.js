9de7b13e18d808a18998781bf3c61eb47ca53912
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2023-06-08 21:12:06 +0530

    Drop feature added

--- drag-drop.js
+++ drag-drop.js
@@ -1,6 +1,8 @@
 const images = document.getElementsByClassName("imgBox");
 
-const boxes = document.getElementsByClassName("whiteBox");
+const boxes = document.getElementsByClassName("dragBox");
+
+const div = document.querySelector('.notify');
 
 for (image of images) {
   image.addEventListener("dragstart", (e) => {
@@ -11,25 +13,45 @@ for (image of images) {
     }, 0);
   });
 
-  image.addEventListener("dragend", () => {
+  image.addEventListener("dragend", (e) => {
     console.log("Drag ended");
+    e.target.className = 'imgBox'
   });
 }
 
 for (box of boxes) {
-  box.addEventListener("dragover", () => {
-    console.log("DragOver");
+  box.addEventListener("dragover", (e) => {
+    e.preventDefault()
+    console.log("DragOver has been triggered");
   });
 
-  box.addEventListener("dragenter", () => {
-    console.log("DragOver");
+  box.addEventListener("dragenter", (e) => {
+    console.log("Dragenter has been triggered");
+    e.target.className += ' dashed'
   });
 
-  box.addEventListener("dragleave", () => {
-    console.log("DragOver");
+  box.addEventListener("dragleave", (e) => {
+    console.log("dragleave has been triggered");
+    e.target.className = 'dragBox'
   });
 
-  box.addEventListener("drop", () => {
-    console.log("DragOver");
+  box.addEventListener("drop", (e) => {
+    console.log("drop has been triggered");
+    e.target.append(image)
+    // setTimeout(() => {
+    //     e.target.className = "imgBox dragBox";
+    //   }, 0);
+    div.textContent = 'Image dropped successfully'
+    div.className = 'notify'
+
+    setTimeout(() => {
+        div.className = 'hide'
+    }, 2000);
+
   });
+}
+
+function reset(){
+    image.className = 'imgBox'
+    box.className = 'dragBox'
 }
